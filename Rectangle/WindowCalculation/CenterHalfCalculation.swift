7f181bc2de09f84d46236c20167eccb18f8b4047
pkrayzy <75462176+pkrayzy@users.noreply.github.com>
2022-07-15 16:04:32 -0400

    Update CenterHalfCalculation.swift

--- Rectangle/WindowCalculation/CenterHalfCalculation.swift
+++ Rectangle/WindowCalculation/CenterHalfCalculation.swift
@@ -10,6 +10,21 @@ import Foundation
 
 class CenterHalfCalculation: WindowCalculation, OrientationAware, RepeatedExecutionsInThirdsCalculation {
     
+    let almostMaximizeHeight: CGFloat
+    let almostMaximizeWidth: CGFloat
+
+    override init() {
+        let defaultHeight = Defaults.almostMaximizeHeight.value
+        almostMaximizeHeight = (defaultHeight <= 0 || defaultHeight > 1)
+            ? 0.9
+            : CGFloat(defaultHeight)
+
+        let defaultWidth = Defaults.almostMaximizeWidth.value
+        almostMaximizeWidth = (defaultWidth <= 0 || defaultWidth > 1)
+            ? 0.9
+            : CGFloat(defaultWidth)
+    }
+    
     func calculateFractionalRect(_ params: RectCalculationParameters, fraction: Float) -> RectResult {
         
         let visibleFrameOfScreen = params.visibleFrameOfScreen
@@ -31,12 +46,12 @@ class CenterHalfCalculation: WindowCalculation, OrientationAware, RepeatedExecut
         var rect = visibleFrameOfScreen
         
         // Resize
-        rect.size.height = visibleFrameOfScreen.height
+        rect.size.height = round(visibleFrameOfScreen.height * almostMaximizeHeight)
         rect.size.width = round(visibleFrameOfScreen.width * CGFloat(fraction))
         
         // Center
         rect.origin.x = round((visibleFrameOfScreen.width - rect.width) / 2.0) + visibleFrameOfScreen.minX
-        rect.origin.y = round((visibleFrameOfScreen.height - rect.height) / 2.0) + visibleFrameOfScreen.minY
+        rect.origin.y = round((visibleFrameOfScreen.height - calculatedWindowRect.height) / 2.0) + visibleFrameOfScreen.minY
         
         return RectResult(rect, subAction: .centerVerticalHalf)
     }
@@ -46,11 +61,11 @@ class CenterHalfCalculation: WindowCalculation, OrientationAware, RepeatedExecut
         
         // Resize
         rect.size.width = visibleFrameOfScreen.width
-        rect.size.height = round(visibleFrameOfScreen.height * CGFloat(fraction))
+        rect.size.height = round(visibleFrameOfScreen.height * almostMaximizeHeight)
         
         // Center
         rect.origin.x = round((visibleFrameOfScreen.width - rect.width) / 2.0) + visibleFrameOfScreen.minX
-        rect.origin.y = round((visibleFrameOfScreen.height - rect.height) / 2.0) + visibleFrameOfScreen.minY
+        rect.origin.y = round((visibleFrameOfScreen.height - calculatedWindowRect.height) / 2.0) + visibleFrameOfScreen.minY
         
         return RectResult(rect, subAction: .centerHorizontalHalf)
     }
