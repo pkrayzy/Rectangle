b1feb593f53a5ab6819f954c7af98d70a23b9b0f
pkrayzy <75462176+pkrayzy@users.noreply.github.com>
2023-08-24 23:35:10 -0400

    Update SecondFourthCalculation.swift

--- Rectangle/WindowCalculation/SecondFourthCalculation.swift
+++ Rectangle/WindowCalculation/SecondFourthCalculation.swift
@@ -41,8 +41,13 @@ class SecondFourthCalculation: WindowCalculation, OrientationAware {
         
     func landscapeRect(_ visibleFrameOfScreen: CGRect) -> RectResult {
         var rect = visibleFrameOfScreen
+
+        rect.size.height = floor(visibleFrameOfScreen.height * 0.975)
+        rect.origin.y = round(visibleFrameOfScreen.height * 0.0125)
+
         rect.size.width = floor(visibleFrameOfScreen.width / 4.0)
         rect.origin.x = visibleFrameOfScreen.minX + rect.width
+        
         return RectResult(rect, subAction: .centerLeftFourth)
     }
     
